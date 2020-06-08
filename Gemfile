1b746e01ca49709cffe9a3559973330d55dc2e34
nakaya-kousuke <i.my.me.ode23@gmail.com>
2020-06-08 23:19:15 +0900

    setting s3

--- Gemfile
+++ Gemfile
@@ -63,3 +63,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'haml-rails'
 gem 'pry-rails'
 gem 'devise'
+gem 'fog-aws'
+gem 'carrierwave'
+gem 'rmagick'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200607092409) do
+ActiveRecord::Schema.define(version: 20200608134633) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20200607092409) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "image"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
