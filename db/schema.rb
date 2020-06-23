685edd1c91b8b6a505076c644db539625ed2ed66
akira1239 <akira_ota1239@icloud.com>
2020-06-23 22:13:28 +0900

    create-signup/signin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200608134633) do
+ActiveRecord::Schema.define(version: 20200621105335) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
@@ -21,6 +21,17 @@ ActiveRecord::Schema.define(version: 20200608134633) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "image"
+    t.string   "nickname",                            null: false
+    t.string   "password",                            null: false
+    t.string   "password_conform",                    null: false
+    t.string   "family_name",                         null: false
+    t.string   "first_name",                          null: false
+    t.string   "family_name_kana",                    null: false
+    t.string   "first_name_kana",                     null: false
+    t.integer  "birth_year",                          null: false
+    t.integer  "birth_month",                         null: false
+    t.integer  "birth_day",                           null: false
+    t.string   "phone_num"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
