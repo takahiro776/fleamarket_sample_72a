4cc574051c354bd8ae7f078fda9d1b22a78bf03e
akira1239 <akira_ota1239@icloud.com>
2020-07-07 23:31:24 +0900

    signin/signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,10 @@ class UsersController < ApplicationController
   end
 
   private
+  def user_params
 
-    params.require(:user).permit(:nickname, :text, :password)
-
+    params.require(:user).permit(:nickname, :txet )
+   
   end
 
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,5 @@
 class Address < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   validates :postal_code, :prefectures, :ctiy, :block_number,presence: true
-  validates :postal_code,format: {with: /\A\d{3}[_]\d{4}\z/}
+  # validates :postal_code,format: {with: /\A\d{3}[_]\d{4}\z/}
 end
