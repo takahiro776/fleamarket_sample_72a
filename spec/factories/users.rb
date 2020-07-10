2830dd518c765d8acdc4bf8c9c7975c8140174fd
takahiro776 <homareendo@gmail.com>
2020-07-10 19:04:23 +0900

    不要なファイルの削除とユーザーモデルの単体テスト

---
+++ spec/factories/users.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+
+  factory :user do
+    email              {"aaa@aaaa"}
+    nickname           {"hiro"}
+    password              {"aaaa1111"}
+    password_conform              {"aaaa1111"}
+    family_name              {"山田"}
+    first_name              {"彩"}
+    family_name_kana              {"ヤマダ"}
+    first_name_kana              {"アヤ"}
+    birth_year              {"1991"}
+    birth_month              {"12"}
+    birth_day              {"24"}
+  end
+  
+end
