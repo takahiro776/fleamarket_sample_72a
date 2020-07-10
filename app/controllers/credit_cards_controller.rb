2f924b3e95039475348c00d40715b8cb94281863
takahiro776 <homareendo@gmail.com>
2020-07-10 18:59:22 +0900

    クレジットカードの登録、商品購入のエラーハンドリング

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -5,47 +5,50 @@ class CreditCardsController < ApplicationController
   def new
   end
 
+  def brandnew
+  end
+
   def create
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     customer = Payjp::Customer.create(
       card: params[:card_token],
       metadata: {user_id: current_user.id}
     )
-
     @card = CreditCard.new(
       card_id: customer.default_card,
       customer_id: customer.id,
       user_id: current_user.id
     )
-    # binding.pry
     if @card.save
-      redirect_to action: "show",  id: @card.user_id
+      redirect_to action: "show", id: @card.user_id
+      flash[:notice] = "カードの登録が完了しました"
     else
-      redirect_to action: "new"
+      render :new
     end
   end
 
-  def show #Cardのデータpayjpに送り情報を取り出します
-    # card = CreditCard.where(user_id: current_user.id).first
-    # if card.blank?
-    #   redirect_to action: "new" 
-    # else
-    #   Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-    #   customer = Payjp::Customer.retrieve(card.customer_id)
-    #   @default_card_information = customer.cards.retrieve(card.card_id)
-    # end
+  def show 
+    card = CreditCard.where(user_id: current_user.id).last
+    if card.blank?
+      redirect_to action: "brandnew" 
+    else
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      @card_info = customer.cards.retrieve(card.card_id)
+    end
   end
 
-  # def deleate #顧客データの削除
-  #   card = Card.where(user_id: current_user.id).first
-  #   if card.blank?
-  #   else
-  #     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
-  #     customer = Payjp::Customer.retrieve(card.customer_id)
-  #     customer.delete
-  #     card.delete
-  #   end
-  #   redirect_to action: "new"
-  # end
+  def delete
+    card = CreditCard.where(user_id: current_user.id).last
+    if card.blank?
+    else
+      Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      card.delete
+      customer.delete
+    end
+    redirect_to action: "show"
+    flash[:notice] = "カード情報を削除しました"
+  end
 
 end
