4425bec79cb82687b6203079a53d77010712d419
nakaya-kousuke <i.my.me.ode23@gmail.com>
2020-07-10 10:56:12 +0900

    add save-images
    画像保存の途中です！

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,7 @@
 class Image < ApplicationRecord
   belongs_to :item, optional: true
 
-  mount_uploader :image_url, ImageUploader
+  mount_uploaders :image_url, ImageUploader
 
   validates :image_url, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,12 +14,9 @@ Rails.application.routes.draw do
 
   get 'users/show'
 
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "items#index"
   resources :items, only:[:new, :show, :create]
-  
-  resources :items, only:[:new, :show]
   resources :users, only:[:show]
   resources :transaction, only:[:new]
   resources :credit_cards, only: [:new, :create, :show, :delete]
