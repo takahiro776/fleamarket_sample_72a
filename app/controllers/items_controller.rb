a85a124bf757e0bf694f31feb2ecbf930f95cca2
nakaya-kousuke <i.my.me.ode23@gmail.com>
2020-07-12 13:48:35 +0900

    save image
    画像の保存ができるようになりました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,9 +10,9 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    binding.pry
-    # if params[:item][:images_attributes] && @item.save!
-    if @item.save!
+    # binding.pry
+    if params[:item][:images_attributes] && @item.save
+    # if @item.save
       redirect_to root_path
     else
       @item.images.new
@@ -35,7 +35,7 @@ class ItemsController < ApplicationController
                   :delivery_regions,
                   :shipping_schedule,
                   :price,
-                  images_attributes: {image_url: []})
+                  images_attributes: [:image_url, :id, :_destroy])
           .merge(user_id: current_user.id)
   end
 end
