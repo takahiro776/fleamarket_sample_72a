832b0f89fb258dcfc78fef7ede019c68806a9dc3
nakaya-kousuke <i.my.me.ode23@gmail.com>
2020-07-16 22:21:05 +0900

    fix master

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200709115727) do
+ActiveRecord::Schema.define(version: 20200716114117) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "postal_code",           limit: 7, null: false
@@ -70,6 +70,15 @@ ActiveRecord::Schema.define(version: 20200709115727) do
     t.index ["user_id"], name: "index_items_on_user_id", using: :btree
   end
 
+  create_table "transactions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "item_id",    null: false
+    t.integer  "user_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_transactions_on_item_id", using: :btree
+    t.index ["user_id"], name: "index_transactions_on_user_id", using: :btree
+  end
+
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -96,4 +105,6 @@ ActiveRecord::Schema.define(version: 20200709115727) do
   add_foreign_key "credit_cards", "users"
   add_foreign_key "images", "items"
   add_foreign_key "items", "categories"
+  add_foreign_key "transactions", "items"
+  add_foreign_key "transactions", "users"
 end
