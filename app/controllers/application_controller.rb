75c32cd9036c244ffb4ded92da09565c2d3fbc58
akira1239 <akira_ota1239@icloud.com>
2020-07-23 14:52:48 +0900

    signup/signin fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:family_name, :first_name, :family_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day,])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :encrypted_password, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day, :telephone, :text, :icon])
   end
 
   def production?
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,8 +20,7 @@ class UsersController < ApplicationController
   private
   def user_params
 
-    params.require(:user).permit(:nickname, :txet )
-   
+    params.require(:user).permit(:nickname, :text, :password)
   end
 
 end
