c85d13748b56bcbd5e5c5f006eaa80cb4ea53312
nakaya-kousuke <i.my.me.ode23@gmail.com>
2020-08-13 20:30:31 +0900

    Basic認証の追加
    なぜかBasic認証が消えていた。。。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception 一旦コメントアウトしてます
-  # before_action :basic_auth, if: :production?
+  before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -10,9 +10,16 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:family_name, :first_name, :family_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day,])
   end
 
+  private
+
   def production?
     Rails.env.production?
   end
 
-  
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
+
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,6 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
-    # @images = Image.all
   end
 
   def new
